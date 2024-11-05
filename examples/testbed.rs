ae8795694b0b0a3b7998edc4df0d4c241dba7ea5
Emre AYDIN <aeaydin1@gmail.com>
2024-11-05 15:45:33 -0500

    Validation layers added

--- examples/testbed.rs
+++ examples/testbed.rs
@@ -1,50 +1,63 @@
-use anyhow::Context;
-use ash::{ext::metal_objects, khr, mvk, prelude::VkResult, vk, Entry, Instance};
+use anyhow::{anyhow, Context};
+use ash::{
+    ext, khr, mvk,
+    vk::{self, EXT_DEBUG_UTILS_NAME},
+    Entry, Instance,
+};
 use glfw::{Action, Context as GlfwContext, Key};
 use raw_window_handle::{HasDisplayHandle, HasWindowHandle};
 
-fn create_instance(entry: &Entry) -> VkResult<Instance> {
-    let app_name = unsafe { std::ffi::CStr::from_bytes_with_nul_unchecked(b"forge\0") };
-    let layers = unsafe {
-        let layer_names = [std::ffi::CStr::from_bytes_with_nul_unchecked(
-            b"VK_LAYER_KHRONOS_validation\0",
-        )];
-        layer_names
-            .iter()
-            .map(|name| name.as_ptr())
-            .collect::<Vec<_>>()
+const VALIDATION_ENABLED: bool = cfg!(debug_assertions);
+const VALIDATION_LAYER: &std::ffi::CStr =
+    unsafe { std::ffi::CStr::from_bytes_with_nul_unchecked(b"VK_LAYER_KHRONOS_validation\0") };
+#[cfg(any(target_os = "macos", target_os = "ios"))]
+const MACOS_REQUIRED_EXTENSIONS: &[*const i8] = &[
+    vk::EXT_METAL_SURFACE_NAME.as_ptr(),
+    khr::portability_enumeration::NAME.as_ptr(),
+    khr::get_physical_device_properties2::NAME.as_ptr(),
+    mvk::macos_surface::NAME.as_ptr(),
+];
+
+unsafe extern "system" fn vulkan_debug_callback(
+    message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+    message_type: vk::DebugUtilsMessageTypeFlagsEXT,
+    p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT<'_>,
+    _user_data: *mut std::os::raw::c_void,
+) -> vk::Bool32 {
+    let callback_data = *p_callback_data;
+    let message_id_name = if callback_data.p_message_id_name.is_null() {
+        std::borrow::Cow::from("")
+    } else {
+        std::ffi::CStr::from_ptr(callback_data.p_message_id_name).to_string_lossy()
+    };
+    let message = if callback_data.p_message.is_null() {
+        std::borrow::Cow::from("")
+    } else {
+        std::ffi::CStr::from_ptr(callback_data.p_message).to_string_lossy()
     };
 
-    let mut extensions = vec![];
-    #[cfg(any(target_os = "macos", target_os = "ios"))]
-    {
-        extensions.push(vk::EXT_METAL_SURFACE_NAME.as_ptr());
-        extensions.push(khr::surface::NAME.as_ptr());
-        extensions.push(khr::portability_enumeration::NAME.as_ptr());
-        extensions.push(khr::get_physical_device_properties2::NAME.as_ptr());
-        extensions.push(mvk::macos_surface::NAME.as_ptr());
+    let formatted_message = format!(
+        "[{:?}] [{}] ({}): {}",
+        message_type, message_id_name, callback_data.message_id_number, message
+    );
+
+    match message_severity {
+        s if s.contains(vk::DebugUtilsMessageSeverityFlagsEXT::ERROR) => {
+            log::error!("{}", formatted_message);
+        }
+        s if s.contains(vk::DebugUtilsMessageSeverityFlagsEXT::WARNING) => {
+            log::warn!("{}", formatted_message);
+        }
+        s if s.contains(vk::DebugUtilsMessageSeverityFlagsEXT::INFO) => {
+            log::info!("{}", formatted_message);
+        }
+        s if s.contains(vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE) => {
+            log::debug!("{}", formatted_message);
+        }
+        _ => log::trace!("{}", formatted_message),
     }
 
-    let appinfo = vk::ApplicationInfo::default()
-        .application_name(app_name)
-        .application_version(0)
-        .engine_name(app_name)
-        .engine_version(0)
-        .api_version(vk::make_api_version(0, 1, 0, 0));
-
-    let create_flags = if cfg!(any(target_os = "macos", target_os = "ios")) {
-        vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
-    } else {
-        vk::InstanceCreateFlags::default()
-    };
-
-    let create_info = vk::InstanceCreateInfo::default()
-        .application_info(&appinfo)
-        .enabled_layer_names(&layers)
-        .enabled_extension_names(&extensions)
-        .flags(create_flags);
-
-    unsafe { entry.create_instance(&create_info, None) }
+    vk::FALSE
 }
 
 fn i8_array_to_string(slice: &[i8]) -> String {
@@ -58,11 +71,105 @@ fn i8_array_to_string(slice: &[i8]) -> String {
     .to_string()
 }
 
+fn create_instance(
+    entry: &Entry,
+    debug_info: &mut vk::DebugUtilsMessengerCreateInfoEXT,
+) -> anyhow::Result<Instance> {
+    let app_name = unsafe { std::ffi::CStr::from_bytes_with_nul_unchecked(b"forge\0") };
+    let layers = get_required_layers(entry)?;
+    let extensions = get_required_extensions(entry)?;
+    let version = unsafe { entry.try_enumerate_instance_version()? }.unwrap_or(vk::API_VERSION_1_0);
+
+    let appinfo = vk::ApplicationInfo::default()
+        .application_name(app_name)
+        .application_version(0)
+        .engine_name(app_name)
+        .engine_version(0)
+        .api_version(version);
+
+    let create_flags = if cfg!(any(target_os = "macos", target_os = "ios")) {
+        vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+    } else {
+        vk::InstanceCreateFlags::default()
+    };
+
+    let mut create_info = vk::InstanceCreateInfo::default()
+        .application_info(&appinfo)
+        .enabled_layer_names(&layers)
+        .enabled_extension_names(&extensions)
+        .flags(create_flags);
+    if VALIDATION_ENABLED {
+        create_info = create_info.push_next(debug_info);
+    }
+
+    Ok(unsafe { entry.create_instance(&create_info, None)? })
+}
+
+fn create_debug_utils(
+    entry: &Entry,
+    instance: &Instance,
+    debug_info: vk::DebugUtilsMessengerCreateInfoEXT,
+) -> anyhow::Result<Option<(ext::debug_utils::Instance, vk::DebugUtilsMessengerEXT)>> {
+    if !VALIDATION_ENABLED {
+        return Ok(None);
+    }
+
+    let debug_utils_loader = ext::debug_utils::Instance::new(entry, instance);
+    let debug_call_back =
+        unsafe { debug_utils_loader.create_debug_utils_messenger(&debug_info, None)? };
+    Ok(Some((debug_utils_loader, debug_call_back)))
+}
+
+fn get_required_layers(entry: &Entry) -> Result<Vec<*const i8>, anyhow::Error> {
+    let layer_names = unsafe {
+        entry
+            .enumerate_instance_layer_properties()?
+            .iter()
+            .map(|layer| std::ffi::CStr::from_ptr(layer.layer_name.as_ptr()))
+            .collect::<Vec<_>>()
+    };
+    let layers = {
+        if VALIDATION_ENABLED && layer_names.contains(&VALIDATION_LAYER) {
+            [VALIDATION_LAYER].iter().map(|l| l.as_ptr()).collect()
+        } else {
+            vec![]
+        }
+    };
+    Ok(layers)
+}
+
+fn get_required_extensions(entry: &Entry) -> Result<Vec<*const i8>, anyhow::Error> {
+    let mut required_extensions = vec![khr::surface::NAME.as_ptr()];
+    if VALIDATION_ENABLED {
+        required_extensions.push(EXT_DEBUG_UTILS_NAME.as_ptr());
+        log::info!("Adding {:?}", EXT_DEBUG_UTILS_NAME);
+    }
+    #[cfg(any(target_os = "macos", target_os = "ios"))]
+    required_extensions.extend_from_slice(MACOS_REQUIRED_EXTENSIONS);
+    let system_extensions_names = unsafe {
+        entry
+            .enumerate_instance_extension_properties(None)?
+            .iter()
+            .map(|extension| std::ffi::CStr::from_ptr(extension.extension_name.as_ptr()))
+            .collect::<Vec<_>>()
+    };
+    for required_extension in &required_extensions {
+        let extension_name = unsafe { std::ffi::CStr::from_ptr(*required_extension) };
+        if !system_extensions_names.contains(&extension_name) {
+            return Err(anyhow!(
+                "extension {} not supported by the system",
+                extension_name.to_string_lossy()
+            ));
+        }
+    }
+    Ok(required_extensions)
+}
+
 fn create_surface(
     entry: &Entry,
     instance: &Instance,
     handle: &(impl HasDisplayHandle + HasWindowHandle),
-) -> anyhow::Result<vk::SurfaceKHR> {
+) -> anyhow::Result<(khr::surface::Instance, vk::SurfaceKHR)> {
     let raw_display_handle = handle
         .display_handle()
         .expect("failed to get raw display handle")
@@ -72,13 +179,16 @@ fn create_surface(
         .expect("failed to get raw window handle")
         .as_raw();
     unsafe {
-        Ok(ash_window::create_surface(
-            entry,
-            instance,
-            raw_display_handle,
-            raw_window_handle,
-            None,
-        )?)
+        Ok((
+            khr::surface::Instance::new(entry, instance),
+            ash_window::create_surface(
+                entry,
+                instance,
+                raw_display_handle,
+                raw_window_handle,
+                None,
+            )?,
+        ))
     }
 }
 
@@ -120,15 +230,44 @@ fn main() -> anyhow::Result<()> {
     window.make_current();
 
     let entry = unsafe { Entry::load()? };
-    let instance = create_instance(&entry)?;
-    let physical_device = create_physical_device(&entry, &instance);
-    let surface = create_surface(&entry, &instance, &window)?;
+
+    let mut debug_info = if VALIDATION_ENABLED {
+        let debug_info = vk::DebugUtilsMessengerCreateInfoEXT::default()
+            .message_severity(
+                vk::DebugUtilsMessageSeverityFlagsEXT::ERROR
+                    | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
+                    | vk::DebugUtilsMessageSeverityFlagsEXT::INFO,
+            )
+            .message_type(
+                vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+                    | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION
+                    | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE,
+            )
+            .pfn_user_callback(Some(vulkan_debug_callback));
+        debug_info
+    } else {
+        vk::DebugUtilsMessengerCreateInfoEXT::default()
+    };
+
+    let instance = create_instance(&entry, &mut debug_info)?;
+    let debug_handles = create_debug_utils(&entry, &instance, debug_info)?;
+
+    let physical_device = create_physical_device(&entry, &instance)?;
+    let (surface_instance, surface) = create_surface(&entry, &instance, &window)?;
 
     while !window.should_close() {
         glfw.poll_events();
         for (_, event) in glfw::flush_messages(&events) {
             handle_window_event(&mut window, event);
         }
+    }
+
+    unsafe {
+        if let Some((debug_instance, debug_callback)) = debug_handles {
+            debug_instance.destroy_debug_utils_messenger(debug_callback, None);
+        }
+        surface_instance.destroy_surface(surface, None);
+        instance.destroy_instance(None);
     }
     Ok(())
 }
